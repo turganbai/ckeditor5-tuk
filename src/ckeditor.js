94d0214371aac3843bf27b35de1ff7ef77a96a2a
Сатыбалдиев Турганбай <turganbay.030282@gmail.com>
2019-09-20 17:48:10 +0600

    --

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,8 +11,7 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-// import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Heading from '../plugins/Heading/src/heading';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -24,19 +23,15 @@ import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
-// import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import ShowTools from '../plugins/ShowTools';
 import CustomHeading from '../plugins/CustomHeading';
-// import HeadingButtonsUI from '../plugins/Heading/src/headingbuttonsui';
 
-export default class ClassicEditor extends ClassicEditorBase {
-}
+export default class ClassicEditor extends ClassicEditorBase {}
 
-// Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	UploadAdapter,
@@ -55,8 +50,6 @@ ClassicEditor.builtinPlugins = [
 	Link,
 	List,
 	MediaEmbed,
-	// Paragraph,
-	// HeadingButtonsUI
 	PasteFromOffice,
 	Table,
 	TableToolbar,
@@ -65,7 +58,6 @@ ClassicEditor.builtinPlugins = [
 	ShowTools,
 ];
 
-// Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
@@ -83,10 +75,6 @@ ClassicEditor.defaultConfig = {
 			'undo',
 			'redo',
 			'alignment',
-			// 'paragraph',
-			// 'heading1',
-			// 'heading2',
-			// 'heading3',
 			'show_tools',
 		]
 	},
@@ -105,6 +93,5 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en',
 };
